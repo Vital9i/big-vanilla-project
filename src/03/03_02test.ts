099c18d0dce3a93598c72daa5a47cbcfb0af148b
Vital9i <vitalikdoiniak@gmail.com>
2024-11-22 21:47:30 +0300

    tests lesson 3(functions)

--- src/03/03_02test.ts
+++ src/03/03_02test.ts
@@ -93,3 +93,35 @@ test('Greeting message shoult be correcting', ()=> {
     expect(message).toBe('Hello New York well be here all 1000000')
 })
 
+// test('Budget should be changed for HOSPITAL', () => {
+//     expect(city.governmentBuildings[0].budget).toBe(200000)
+//     changeBudget(city.governmentBuildings[0], 100000)
+//     expect(city.governmentBuildings[0].budget).toBe(300000)
+// })
+
+// test('Budget should be changed for FIRE-STATION', () => {
+//     expect(city.governmentBuildings[1].budget).toBe(500000)
+//     changeBudget(city.governmentBuildings[0], -100000)
+//     expect(city.governmentBuildings[1].budget).toBe(400000)
+// })
+
+// test('House should be repaired', () => {
+//     expect(city.houses[1].repaired).toBe(false)
+//     repairHouse(city.houses[1])
+//     expect(city.houses[1].repaired).toBe(true)
+// })
+
+// test('staff should be increased', () => {
+//     toFireStaff(city, 20)
+//     expect(city.governmentBuildings[0].staffCount).toBe(180)
+// })
+
+// test('staff should be hire', () => {
+//     toHireStaff(city, 20)
+//     expect(city.governmentBuildings[0].staffCount).toBe(220)
+// })
+
+// test('Greeting message shoult be correcting', ()=> {
+//     const message = createMessage(city)
+//     expect(message).toBe('Hello New York well be here all 1000000')
+// })
